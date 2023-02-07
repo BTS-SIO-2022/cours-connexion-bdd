198c5741c5282df598b133ad05e42ae855ae8681
mathilderenversade <mathirenversade@gmail.com>
2023-02-07 13:09:49 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Pour pouvoir manipuler mes bases de données via PHP, je vais devoir réaliser un certain nombre d'étapes.
+
+ETAPE 1 : Choisir avec quelle extension PHP, je vais me connecter
+
+- extension PHP PDO (PHP DATA OBJECT)
+https://www.php.net/manual/fr/book.pdo.php
+
+- extension MySQLi
+https://www.php.net/manual/fr/book.mysqli.php
+
+Attention, il existe aussi l'extension PHP MySQL mais elle est aujourd'hui dépréciée et dangereuse à utiliser.
+*/
+
+
+
+
+;?>
